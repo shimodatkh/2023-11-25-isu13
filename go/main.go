006332c06fd2ce7cf8fea971f3d20cea5e50ad9d
shimodatkh <tech150220@gmail.com>
2023-11-25 17:29:58 +0900

    SetMaxOpenConns(50)

--- go/main.go
+++ go/main.go
@@ -98,7 +98,7 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(50)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
